f4458eb705d55d8744b6596b3264b28f8583babc
jklynch <jlynch@bnl.gov>
2020-11-25 17:24:44 -0500

    continuing development

---
+++ ophyd_addon/run_pe_count.py
@@ -0,0 +1,59 @@
+import argparse
+import logging
+
+from bluesky import RunEngine
+from bluesky.callbacks.best_effort import BestEffortCallback
+from bluesky.plans import count
+from databroker import catalog
+
+from ophyd_addon.areadetector.document_builders import NewPerkinElmerDetector
+from ophyd_addon.perkin_elmer_diffraction_plan import pe_count
+
+logging.getLogger("TiffWriter").setLevel(logging.DEBUG)
+logging.getLogger("TiffWriter").addHandler(logging.StreamHandler())
+
+
+def run():
+    """
+    # setup for simulated IOC
+    EPICS_CA_ADDR_LIST=127.0.0.1:5064
+    EPICS_CA_AUTO_ADDR_LIST=NO
+
+    Run mongo.
+
+    Run simulated IOC.
+    """
+    # arg_parser = argparse.ArgumentParser()
+    # arg_parser.add_argument("--agent-name", required=True, type=str)
+    # arg_parser.add_argument("--episode-count", required=True, type=int)
+
+    # args = arg_parser.parse_args()
+
+    RE = RunEngine()
+
+    bec = BestEffortCallback()
+
+    RE.subscribe(bec)
+
+    db = catalog["mad"]  # this is set up by entrypoint
+
+    RE.subscribe(db.v1.insert)
+
+    tiff_sim_detector = NewPerkinElmerDetector(
+        prefix="Sim{{det1}}:", name="tiff_sim_detector"
+    )
+
+    RE(
+        pe_count(
+            filename="",
+            exposure=1,
+            num_images=1,
+            num_dark_images=1,
+            num_repetitions=5,
+            delay=2,
+        )
+    )
+
+
+if __name__ == "__main__":
+    run()
